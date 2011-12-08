9efa51b7571be5a5eb1f8f1f95d4fb5b0e6e31ea
Mr Speaker <mrspeaker@gmail.com>
2011-12-08 13:25:36 +0100

    Update ga settings

--- stuartspeaker/footer.php
+++ stuartspeaker/footer.php
@@ -47,12 +47,15 @@
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/scripts/jquery.oneup.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/scripts/speaker.js"></script>
 <script type="text/javascript">
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	try {
-	    var pageTracker = _gat._getTracker("UA-8233321-1");
-	    pageTracker._trackPageview();
-	} catch(err) {}
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-8233321-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 </body><!-- end trasmission -->
 </html>
